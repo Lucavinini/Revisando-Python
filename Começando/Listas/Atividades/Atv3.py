cfb776a707d8cdcf852421daca1349f92725a191
Lucas <vinicioslucas37@hotmail.com>
2025-03-03 13:03:24 -0300

    Terceira onda

---
+++ Começando/Listas/Atividades/Atv3.py
@@ -0,0 +1,13 @@
+#Todas as atividades são retiradas do livro: Curso Intensivo de Python - Escrito por Eric Matthes
+
+#3.3
+#Pense em seu meio de transporte preferido, como
+#motocicleta ou carro, e crie uma lista que armazene vários exemplos. Utilize sua
+#lista para exibir uma série de frases sobre esses itens, como “Gostaria de ter
+#uma moto Honda”.
+
+transportes = ["Moto","Carro","Avião"]
+
+frases = ["Eu amo andar de " + transportes[0] +" Roxa"]
+
+print(frases[0])
---
+++ Começando/TiposDeDados/inteiros.py
@@ -0,0 +1,11 @@
+#Todo dado do tipo inteiro segue essa estrutura definida pelo programador:
+
+a = 32 
+b = 32
+
+y = a + b
+
+print(y)
+
+
+
