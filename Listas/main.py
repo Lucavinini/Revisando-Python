56e0fb8554e311eaf46b62fbd028fc4f87de9735
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-08-29 16:23:51 +0000

    Finalizando Lista

--- Listas/main.py
+++ Listas/main.py
@@ -3,6 +3,9 @@ def ExibirMenu():
     print("1. Exibir lista")
     print("2. Adicionar na lista")
     print("3. Adicionando na posição")
+    print("4. Mudando elemento")
+    print("5. Descobrindo o tamanho")
+    print("6. Remover")
     print("------------------------------")
 
 Lista = []
@@ -29,6 +32,22 @@ while( cont < 1):
                 posi = int(input())
                 Lista.insert(posi, info)
                 print("Valor adicionado na lista")
+            else:
+                if(op == 4): # Mudando um valor por meio da posição dele
+                    print("Insira a posição que você quer mudar")
+                    op = int(input())
+                    print("Digite o novo valor")
+                    novainfo = input() # Caso a posição não exista, vai dar erro e o código encerra
+                    Lista[op] = novainfo
+                    print("Valor atualizado!")
+                else:
+                    if(op == 5): # Tamanho da minha lista usando o len()
+                        print("O tamanho da lista é" , len(Lista))
+                    else:
+                        if(op == 6): #Removendo com o del
+                            print("Digite a posição que quer remover")
+                            op = int(input())
+                            del Lista[op]
 
 
 
---
+++ Trabalhando Com Listas/InterandoComOFor.py
@@ -0,0 +1,8 @@
+Lista = ["lucas", "ana", "teldo"]
+
+for element in Lista: # Para cada element contido em Lista, printe
+    print(element)
+
+
+for number in range(1,6):
+    print(number)
